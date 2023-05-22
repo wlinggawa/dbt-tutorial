5d3ec31efb30ed456f352fc0d3c8472919a1aac7
Wayan Linggawa <wayan.linggawa@otoklix.com>
2023-05-22 11:19:07 +0700

    modify model review

---
+++ airbnb/models/l2_src/src_reviews.sql
@@ -0,0 +1,23 @@
+with reviews as (
+    select LISTING_ID
+    , DATE
+    , REVIEWER_NAME
+    , SENTIMENT
+    from {{ source('airbnb_raw', 'raw_reviews') }}
+),
+listings as (
+    select ID
+    , NAME 
+    , PRICE
+    from {{ ref('src_listings') }}
+),
+final as (
+    select listings.NAME
+    , listings.PRICE
+    , reviews.DATE
+    , reviews.REVIEWER_NAME
+    , reviews.SENTIMENT
+    from reviews
+    left join listings on reviews.LISTING_ID = listings.ID
+)
+select * from final
